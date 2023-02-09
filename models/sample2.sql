e02557687b280c6430837c8aa0c5f4c6dcb3f1d2
Anusha <110527122+Anusha-Bluecopa@users.noreply.github.com>
2023-02-09 11:03:33 +0530

    Update sample2.sql

--- models/sample2.sql
+++ models/sample2.sql
@@ -1,5 +1,2 @@
-with
-  data1 as (select customer_name as name1,customer_id as id1 from `dev-363813.dev_local_ds.Sheet1`),
-      data2 as (select customer_name as name2,customer_id as id2 from `dev-363813.test_sheets.Sheet1`),
-      final_data as (select data1.id1 as name1,data2.name2 as name2 from data1 join data2 on data1.id1 = data2.id2)
-  select * from final_data
+with tr as (SELECT * FROM `dev-363813.dev_silver.sales_transaction`)
+select * from tr LIMIT 1000
