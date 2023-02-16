b795df6cd6c4124737abea36aeec8b2c1f162d63
Anusha <110527122+Anusha-Bluecopa@users.noreply.github.com>
2023-02-16 18:31:35 +0530

    changed query to redshift

--- models/sample.sql
+++ models/sample.sql
@@ -1,5 +1,1 @@
-with
-  data1 as (select customer_name as name1,customer_id as id1 from `dev-363813.dev_local_ds.Sheet1`),
-      data2 as (select customer_name as name2,customer_id as id2 from `dev-363813.test_sheets.Sheet1`),
-      final_data as (select data1.id1 as name1,data2.name2 as name2 from data1 join data2 on data1.id1 = data2.id2)
-  select * from final_data
+SELECT "order_id" "order_id","order_status" "order_status","order_date" "order_date" FROM "ula-silver"."public"."wsh2" WHERE "order_date"='2023-02-15' GROUP BY 1,2,3;
