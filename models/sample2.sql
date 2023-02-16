8661c51b4a465f32d7bfb16cbca82fc2f74de19c
Anusha <110527122+Anusha-Bluecopa@users.noreply.github.com>
2023-02-16 18:32:04 +0530

    Update sample2.sql

--- models/sample2.sql
+++ models/sample2.sql
@@ -1,2 +1,1 @@
-with tr as (SELECT * FROM `dev-363813.dev_silver.sales_transaction`)
-select * from tr LIMIT 1000
+SELECT "order_id" "order_id","order_status" "order_status","order_date" "order_date" FROM "ula-silver"."public"."wsh2" WHERE "order_date"='2023-02-15' GROUP BY 1,2,3;
