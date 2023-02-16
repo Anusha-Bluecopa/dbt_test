68531ba47924736bfd03a86cb90931fbdb409b48
Anusha <110527122+Anusha-Bluecopa@users.noreply.github.com>
2023-02-16 18:33:08 +0530

    Update sample.sql

--- models/sample.sql
+++ models/sample.sql
@@ -1,1 +1,1 @@
-SELECT "order_id" "order_id","order_status" "order_status","order_date" "order_date" FROM "ula-silver"."public"."wsh2" WHERE "order_date"='2023-02-15' GROUP BY 1,2,3;
+SELECT "order_id" "order_id","order_status" "order_status","order_date" "order_date" FROM "ula-silver"."public"."wsh2" WHERE "order_date"='2023-02-15' GROUP BY 1,2,3
